64c9cf89d2efee0698414e8aea2070ccd0aca99c
Chrisanto76 <antonypoure2@gmail.com>
2023-07-21 10:22:03 +0200

    add handlesubmit

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,27 @@ function App() {
 		{ id: nanoid(8), content: 'item-2' },
 		{ id: nanoid(8), content: 'item-3' },
 	]);
+	console.log(todoList);
 
-	function deleteTodo () {
+	const [todo, setTodo] = useState('');
+	const [showValidation, setShowValidation] = useState(false);
 
+	function deleteTodo(id) {
+		setTodoList(todoList.filter((todo) => todo.id !== id));
+	}
+
+	function handleSubmit(e) {
+		//add text to duty
+		e.preventDefault();
+
+		if (todo === '') {
+			setShowValidation(true);
+			return;
+		}
+
+		setTodoList([...todoList, { id: nanoid(), content: todo }]);
+		setTodo('');
+		setShowValidation(false);
 	}
 
 	return (
@@ -18,20 +36,35 @@ function App() {
 			<div className="h-screen bg-slate-900">
 				<div className="max-w-4xl mx-auto pt-20 px-16">
 					<h1 className="text-3xl text-slate-100 mb-4">To-do List</h1>
-					<form className="mb-10">
+					<form onSubmit={handleSubmit} className="mb-10">
 						<label htmlFor="todo-item" className="text-slate-50">
 							Add something
 						</label>
-						<input type="text" className="mt-1 block w-full rounded" />
+						<input
+							value={todo}
+							onChange={(e) => setTodo(e.target.value)}
+							type="text"
+							className="mt-1 block w-full rounded"
+						/>
+						{showValidation && (
+							<p className="text-red-400">Add something before to your duty</p>
+						)}
 						<button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">
 							Add
 						</button>
 					</form>
 					<ul>
-						{todoList.map(item => (
-							<ListItem key={item.id} itemData={item} deleteTodo={deleteTodo} 
-							/>
-						))}
+						{todoList.length === 0 && (
+							<li className="text-slate-50 text-md">Nothing to show...</li>
+						)}
+						{todoList.length > 0 &&
+							todoList.map((item) => (
+								<ListItem
+									key={item.id}
+									itemData={item}
+									deleteTodo={deleteTodo}
+								/>
+							))}
 					</ul>
 				</div>
 			</div>
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-
-export default function ListItem() {
+export default function ListItem({ itemData, deleteTodo }) {
 	return (
 		<li className="p-2 bg-zinc-200 mb-2 rounded flex">
-			<span>item 1</span>
-			<button className="ml-auto bg-red-600 w-6 h-6 rounded text-zinc-200">
+			<span>{itemData.content}</span>
+			<button
+				onClick={() => deleteTodo(itemData.id)}
+				className="ml-auto bg-red-600 w-6 h-6 rounded text-zinc-200"
+			>
 				X
 			</button>
 		</li>
